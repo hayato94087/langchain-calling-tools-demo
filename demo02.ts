dafcdacd29de534011ecc549c1a5b5326b87fa18
hayato94087 <hayato94087@gmail.com>
2024-05-27 09:56:23 +0900

    Toolsを呼び出す

---
+++ demo02.ts
@@ -0,0 +1,48 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { z } from "zod";
+import { DynamicStructuredTool } from "@langchain/core/tools";
+import 'dotenv/config'
+
+// model
+const llm = new ChatOpenAI({
+  model: "gpt-3.5-turbo",
+  temperature: 0
+});
+
+// schema
+const calculatorSchema = z.object({
+  operation: z
+    .enum(["add", "subtract", "multiply", "divide"])
+    .describe("実行する操作の種類。"),
+  number1: z.number().describe("操作する最初の数値。"),
+  number2: z.number().describe("操作する2番目の数値。"),
+});
+
+// tools
+const calculatorTool = new DynamicStructuredTool({
+  name: "calculator",
+  description: "Can perform mathematical operations.",
+  schema: calculatorSchema,
+  func: async ({ operation, number1, number2 }) => {
+    // Functions must return strings
+    if (operation === "add") {
+      return `${number1 + number2}`;
+    } else if (operation === "subtract") {
+      return `${number1 - number2}`;
+    } else if (operation === "multiply") {
+      return `${number1 * number2}`;
+    } else if (operation === "divide") {
+      return `${number1 / number2}`;
+    } else {
+      throw new Error("無効な操作。");
+    }
+  },
+});
+
+const llmWithTools = llm.bindTools([calculatorTool]);
+
+const res = await llmWithTools.invoke("3 * 12 は？");
+
+console.log(res);
+console.log(res.tool_calls);
+
